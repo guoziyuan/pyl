b1d348649fb4dc8ff02ab328eeae5b219a142333
guoziyuan03 <guoziyuan03@kuaishou.com>
2021-10-27 10:03:46 +0800

    修复tuple误解

--- 202110/lession5.py
+++ 202110/lession5.py
@@ -1,9 +1,9 @@
 '''
 元组
 
-元组特点：生成之后，元素不可改变，可以查询
+元组特点：生成之后，元素指向不可改变，可以查询
 元组本身可以被删除
-不存在空元组
+空元组 ()
 '''
 
 '''
@@ -28,3 +28,35 @@ d = b * 5
 print(d)
 
 del a
+
+'''
+理解元组元素指向不变
+
+元组中的元素可以是一个数组，这个数组中的元素是可以改变的
+'''
+g = [1, 2, 3]
+
+g.append(919)
+f = (1, 2, g)
+print(type(g))
+print(f)
+g.append(99)
+print(g)
+print(f)
+g[-1] = 99
+f[2][0] = 100
+print(f)
+f[2].clear()
+print(f)
+f[2].append(90)
+print(f)
+f[2].append(90)
+print(f)
+f[2].append(90)
+print(f)
+f[2].append(90)
+print(f)
+
+print(f[2] == g)
+
+print(g)
